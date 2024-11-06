dc17b2d327fd3773e6a4930a96371de1705c5a2a
Pepolim <37425326+Pepolim@users.noreply.github.com>
2024-11-06 19:41:39 +0000

    Update pygame_teste.py

--- pygame_teste.py
+++ pygame_teste.py
@@ -8,17 +8,59 @@ WIN = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("First Game")
 
+"""
+Constants for the game:
+"""
 WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+BORDER = pygame.Rect(screen_width/2 - 5, 0, 10, screen_height)
 
 FPS = 60
+VEL = 5
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load('assets/spaceship_yellow.png')
+# os.path.join() is used to join the path of the image file with the current directory path. Works across all operating systems.
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), -90)
 
-def draw_window():
+
+def draw_window(red, yellow):
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+# yellow movement
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0: # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x: # RIGHT
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0: # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < screen_height - 15: # DOWW
+        yellow.y += VEL
+        
+# red movement
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width: # LEFT
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < screen_width: # RIGHT
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0: # UP
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < screen_height - 15 : # DOWW
+        red.y += VEL
+
+# main function
 def main():
+    yellow = pygame.Rect(150, 220, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    red = pygame.Rect(750, 220, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    
     # Creates a clock object to control the frame rate of the game loop.
     clock = pygame.time.Clock()
     
@@ -28,9 +70,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
+        
     pygame.quit()
-
 """
 Executes the main function when the script is run directly (not imported as a module).
 """
